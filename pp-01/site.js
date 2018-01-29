b310c04d62b1283d21062ef790d73625972a7480
Guzelnas <gnasybullina@gmail.com>
2018-01-28 22:40:53 -0600

    add error message

--- pp-01/site.js
+++ pp-01/site.js
@@ -3,9 +3,11 @@ $('#contact-form').on("submit", function(e) {
       var phoneValid = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
       e.preventDefault();
       if (!phoneValid.test(phone)) {
-        console.log('Phone number must contain 10 digits')
+        console.log('Wrong phone format')
         $('#invalid').remove();
+        $('#contact-form')append('<li id = "invalid">Phone number must contain 10 digits!</li>');
         return false;
       } else {
+        $('#invalid').remove();
         $(this).html('The form was submitted');
       });
