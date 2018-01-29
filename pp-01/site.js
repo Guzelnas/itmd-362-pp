827d75171f8fc1e0469205a741a9b8a04b5199cc
Guzelnas <gnasybullina@gmail.com>
2018-01-29 10:43:47 -0600

    test

--- pp-01/site.js
+++ pp-01/site.js
@@ -2,12 +2,12 @@ $('#contact-form').on("submit", function(e) {
   var phone = $('#phone').val();
   var phoneValid = /^\d{10}$/;
   e.preventDefault();
-  if (phoneValid.test(phone)) {
-    $('#contact-form').append('The form was submitted');
-  } else {
+  if (!phoneValid.test(phone)) {
     console.log('invalid phone');
     $('#invalid').remove();
     $('#contact-form') append('Phone number must contain 10 digits!');
     return false;
+  } else {
+    $('#contact-form').append('The form was submitted');
   }
 });
