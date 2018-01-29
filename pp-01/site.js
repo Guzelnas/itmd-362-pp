e96aef2a01d6363d4873f0981dd10e2fa402fc9c
Guzelnas <gnasybullina@gmail.com>
2018-01-28 22:30:55 -0600

    add check valid phone format

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,4 +1,11 @@
-$('#contact-form').on("submit", function(e){
-  $(this).html('The form was submitted');
-  e.preventDefault();
-});
+$('#contact-form').on("submit", function(e) {
+      var phone = $('#phone').val();
+      var phoneValid = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+      e.preventDefault();
+      if (!phoneValid.test(phone)) {
+        console.log('Phone number must contain 10 digits')
+        $('#invalid').remove();
+        return false;
+      } else {
+        $(this).html('The form was submitted');
+      });
