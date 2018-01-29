3711fedbb91bb1045a1592bd036baa7fde69c017
Guzelnas <gnasybullina@gmail.com>
2018-01-29 11:50:54 -0600

    add invalid id, validate phone format

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,11 +1,11 @@
 $('#contact-form').on("submit", function(e) {
-  var email = $('#email').val();
-  var emailValid = /.+@.+/;
+  var phone = $('#phone').val();
+  var phoneValid = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
   e.preventDefault();
-  if (!emailValid.test(email)) {
-    console.log('invalid email');
+  if (!phoneValid.test(phone)) {
+    console.log('invalid phone');
     $('#invalid').remove();
-    $('#contact-form').append('Wrong email');
+    $('#contact-form').append('<li id="invalid">Phone must contain 10 digits!</li>');
     return false;
   } else {
     $(this).remove();
