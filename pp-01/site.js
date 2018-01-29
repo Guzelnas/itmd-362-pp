d1a951a33b5f008b7d7c6146f809e1b7f09a4a74
Guzelnas <gnasybullina@gmail.com>
2018-01-28 23:54:59 -0600

    change phoneValid

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,6 +1,6 @@
 $('#contact-form').on("submit", function(e) {
       var phone = $('#phone').val();
-      var phoneValid = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+      var phoneValid = /^\d{10}$/;
       e.preventDefault();
       if (!phoneValid.test(phone)) {
         console.log('Wrong phone format')
@@ -8,7 +8,6 @@ $('#contact-form').on("submit", function(e) {
         $('#contact-form')append('<li id = "invalid">Phone number must contain 10 digits!</li>');
         return false;
       } else {
-        $('#invalid').remove();
         $(this).html('The form was submitted');
       }
     };
