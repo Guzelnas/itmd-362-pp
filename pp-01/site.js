2a1f99960b126ee46a43084410e54a5e2d880494
Guzelnas <gnasybullina@gmail.com>
2018-01-29 10:33:07 -0600

    validate phone number

--- pp-01/site.js
+++ pp-01/site.js
@@ -2,13 +2,13 @@ $('#contact-form').on("submit", function(e) {
   var phone = $('#phone').val();
   var phoneValid = /^\d{10}$/;
   e.preventDefault();
-  if (!phoneValid.test(phone)) {
+  if (phone.value.match(phoneValid)) {
+    $('#contact-form').append('The form was submitted');
+  } else {
+    $(this).remove();
     console.log('Wrong phone format');
     $('#invalid').remove();
     $('#contact-form') append('<li id = "invalid">Phone number must contain 10 digits!</li>');
     return false;
-  } else {
-    $(this).remove();
-    $('#contact-form').append('The form was submitted');
   }
 });
